f388ffe466d2f8511c89e5fcc29886b0abd3c7f7
incara <dio.cletian10@gmail.com>
2022-05-02 12:13:51 -0400

    Minor changes around detecting shark mouth and fin markers and altering perspective.

--- three/js/index.js
+++ three/js/index.js
@@ -1,12 +1,12 @@
 var scene;
-var arToolkitSource, arToolkitContext, smoothedControls;
-var markerRoot1;
+var arToolkitSource, arToolkitContext, sharkFinControlsWrapper, sharkMouthControlsWrapper;
+var sharkFinMarkerRoot, sharkMouthMarkerRoot;
 
 const canvas = document.querySelector("#canvas");
 const width = canvas.clientWidth;
 const height = canvas.clientHeight;
 
-var camera = new THREE.PerspectiveCamera(80, 2, 0.1, 50000);
+var camera = new THREE.PerspectiveCamera(45, screen.width / screen.height, 0.1, 1000);
 
 var renderer = new THREE.WebGLRenderer({
     antialias : true,
@@ -49,23 +49,41 @@ function initialize() {
         camera.projectionMatrix.copy(arToolkitContext.getProjectionMatrix());
     });
 
-    markerRoot1 = new THREE.Group();
-    scene.add(markerRoot1);
+    sharkFinMarkerRoot = new THREE.Group();
+    scene.add(sharkFinMarkerRoot);
 
-    new THREEx.ArMarkerControls(arToolkitContext, markerRoot1, {
+    sharkMouthMarkerRoot = new THREE.Group();
+    scene.add(sharkMouthMarkerRoot);
+
+    new THREEx.ArMarkerControls(arToolkitContext, sharkFinMarkerRoot, {
         type : 'pattern',
         patternUrl : "/resources/patterns/fin.patt"
     });
 
-    var smoothedRoot = new THREE.Group();
-    scene.add(smoothedRoot);
-    smoothedControls = new THREEx.ArSmoothedControls(smoothedRoot, {
+    new THREEx.ArMarkerControls(arToolkitContext, sharkMouthMarkerRoot, {
+        type : 'pattern',
+        patternUrl : "/resources/patterns/mouth.patt"
+    });
+
+    var sharkFinSmoothedRoot = new THREE.Group();
+    scene.add(sharkFinSmoothedRoot);
+    sharkFinControlsWrapper = new THREEx.ArSmoothedControls(sharkFinSmoothedRoot, {
         lerpPosition: 0.8,
         lerpQuaternion: 0.8,
         lerpScale: 1
     });
 
-    loadModel(smoothedRoot);
+    var sharkMouthSmoothedRoot = new THREE.Group();
+    scene.add(sharkMouthSmoothedRoot);
+    sharkMouthControlsWrapper = new THREEx.ArSmoothedControls(sharkMouthSmoothedRoot, {
+        lerpPosition: 0.8,
+        lerpQuaternion: 0.8,
+        lerpScale: 1
+    });
+
+    loadModel(sharkMouthSmoothedRoot);
+    loadModel(sharkFinSmoothedRoot);
+
 }
 
 function onResize() {
@@ -83,11 +101,32 @@ function update(){
         arToolkitContext.update(arToolkitSource.domElement);
     }
     // additional code for smoothed controls
-    smoothedControls.update(markerRoot1);
+    sharkMouthControlsWrapper.update(sharkMouthMarkerRoot);
+    sharkFinControlsWrapper.update(sharkFinMarkerRoot);
+    //console.log(sharkFinControlsWrapper.object3d.children);
+    if(sharkFinControlsWrapper.object3d.visible) {
+        //the marker has been detected and our model is visible
+    }
 }
 
 function render() {
+    if (resizeUpdate()) {
+        const canvas = renderer.domElement;
+        camera.aspect = canvas.clientWidth / canvas.clientHeight;
+        camera.updateProjectionMatrix();
+    }
     renderer.render(scene, camera);
+}
+
+function resizeUpdate() {
+    const canvas = renderer.domElement;
+    const width = canvas.clientWidth;
+    const height = canvas.clientHeight;
+    const needResize = canvas.width !== width || canvas.height !== height;
+    if (needResize) {
+        renderer.setSize(width, height, false);
+    }
+    return needResize;
 }
 
 function animate() {
@@ -101,7 +140,7 @@ function loadModel(smoothedRoot) {
     loader.load(
         '/resources/models/shark-fin.gltf',
         function (gltf) {
-            gltf.scene.scale.set(0.01, 0.01, 0.01);
+            gltf.scene.scale.set(0.05, 0.05, 0.05);
             const root = gltf.scene;
             smoothedRoot.add(root);
         },
